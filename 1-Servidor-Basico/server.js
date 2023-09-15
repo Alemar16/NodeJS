69a32e9918f0ae735ac4066bcf843e35519e7a3a
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-15 18:54:19 -0300

    Se crea Ejercicio 1

---
+++ 1-Servidor-Basico/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get ('/', (req, res) => {
+  res.send('Hola desde mi servidor');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
+
+module.exports = app
