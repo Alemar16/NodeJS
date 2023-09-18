2f1e814725d0aa93814056c4385cddf91152d55d
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-17 21:04:44 -0300

    se configura las rutas en index.js de la app

--- 1-Servidor/1.4-NodeJS-Project_Basic/src/index.js
+++ 1-Servidor/1.4-NodeJS-Project_Basic/src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+//importar routes
+import indexRoutes from "./routes/index.js";
 
 const app = express();
 
-app.get("/", (req, res) => res.send ("Hello World"));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+//console.log(join(__dirname, "views"));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(indexRoutes);
 
 app.listen(3000);
 console.log("Servidor corriendo en el puerto 3000");
