eb360edac0dfa39d60434af1963601af2a185d8d
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-17 22:16:16 -0300

    se agrega ruta de express para public

--- 1-Servidor/1.4-NodeJS-Project_Basic/src/index.js
+++ 1-Servidor/1.4-NodeJS-Project_Basic/src/index.js
@@ -11,7 +11,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(indexRoutes);
+app.use(express.static(join(__dirname, "public")));
 
 app.listen(3000);
 console.log("Servidor corriendo en el puerto 3000");
